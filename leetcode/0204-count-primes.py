57f03f68b48f3b25354bbc507f2e04b898aa9fe8
Rudolf Potucek <potucek@artiverse.net>
2018-12-25 00:12:29 -0700

    Modified prime counter with slightly smaller memory footprint

--- leetcode/0204-count-primes.py
+++ leetcode/0204-count-primes.py
@@ -1,4 +1,5 @@
 class Solution(object):
+    # Generate primes
     def sieveOfErastothenes(self,n):
         """
         :type n: int
@@ -15,6 +16,7 @@ class Solution(object):
             yes_primes.append(ii)
         return yes_primes
 
+    # Count generated prime array - too slow
     def countPrimesWithGeneration(self, n):
         """
         :type n: int
@@ -22,7 +24,8 @@ class Solution(object):
         """
         return len(self.sieveOfErastothenes(n))
 
-    def countPrimes(self, n):
+    # Simplified erastothenes. Count hits using sets - fast enough but too memory intensive
+    def countPrimesWithSets(self, n):
         """
         :type n: int
         :rtype: int
@@ -37,6 +40,24 @@ class Solution(object):
             yes_primes+=1
         return yes_primes
 
+    # Simplified erastothenes saving a bit of memory by using array instead of set
+    #
+    def countPrimes(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        if n<3:
+            return 0
+        yes_primes=[True]*n
+        for ii in xrange(2,n):
+            if yes_primes[ii]:
+                for jj in xrange(2*ii,n,ii):
+                    yes_primes[jj]=False
+        # # We could recover the actual primes here
+        # print [prime for prime,isprime in enumerate(yes_primes) if isprime]
+        return sum(yes_primes)-2
+
 
 x=Solution()
 
@@ -50,6 +71,30 @@ print e==a
 
 q = 1000000
 e = 78498
+print "Question",q
+print "Expect",e
+a = x.countPrimes(q)
+print "Result",a
+print e==a
+
+q = 0
+e = 0
+print "Question",q
+print "Expect",e
+a = x.countPrimes(q)
+print "Result",a
+print e==a
+
+q = 1
+e = 0
+print "Question",q
+print "Expect",e
+a = x.countPrimes(q)
+print "Result",a
+print e==a
+
+q = 2
+e = 0
 print "Question",q
 print "Expect",e
 a = x.countPrimes(q)
